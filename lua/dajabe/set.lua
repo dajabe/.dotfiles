02fb12c9bfedaf52a86b4394ec0d073dd078290f
David Benton <djb@dajabe.nz>
2024-04-13 01:29:07 +1200

    Setup harpoon and split plugin configs into their own files

---
+++ lua/dajabe/set.lua
@@ -0,0 +1,57 @@
+-- Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+-- See `:help vim.opt` or `:help option-list`
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+-- Line numbers show both
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = '%s %l %r '
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim. See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+-- See `:help 'list'` and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 16
+vim.opt.colorcolumn = '80'
