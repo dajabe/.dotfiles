e885a0c9a31c67c45b50548ca1c84e7a83d5add1
David Benton <djb@dajabe.nz>
2024-04-15 09:37:22 +1200

    Update nvim oil config to float the window instead and set hidden file defaults

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,5 +1,14 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 10,
+      max_width = 100,
+      max_height = 100,
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
