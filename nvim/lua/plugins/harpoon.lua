f1b43e75720a87d560dacf5eaf0319eca8827e65
David Benton <djb@dajabe.nz>
2024-10-10 17:11:44 +1300

    nvim: add quick close all shortcut and modify harpoon to use branch specific lists

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -17,6 +17,32 @@ local function get_os_command_output(cmd, cwd)
   return stdout, ret, stderr
 end
 
+local function key_name()
+  local branch
+  -- use tpope's fugitive for faster branch name resolution if available
+  if vim.fn.exists '*FugitiveHead' == 1 then
+    branch = vim.fn['FugitiveHead']()
+    -- return "HEAD" for parity with `git rev-parse` in detached head state
+    if #branch == 0 then
+      branch = 'HEAD'
+    end
+  else
+    -- `git branch --show-current` requires Git v2.22.0+ so going with more
+    -- widely available command
+    branch = get_os_command_output({
+      'git',
+      'rev-parse',
+      '--abbrev-ref',
+      'HEAD',
+    })[1]
+  end
+  if branch then
+    return vim.loop.cwd() .. '-' .. branch
+  else
+    return vim.loop.cwd()
+  end
+end
+
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
@@ -28,146 +54,86 @@ return {
     settings = {
       save_on_toggle = true,
       sync_on_ui_close = true,
-      -- key = function()
-      --   local branch
-      --
-      --   -- use tpope's fugitive for faster branch name resolution if available
-      --   if vim.fn.exists '*FugitiveHead' == 1 then
-      --     branch = vim.fn['FugitiveHead']()
-      --     -- return "HEAD" for parity with `git rev-parse` in detached head state
-      --     if #branch == 0 then
-      --       branch = 'HEAD'
-      --     end
-      --   else
-      --     -- `git branch --show-current` requires Git v2.22.0+ so going with more
-      --     -- widely available command
-      --     branch = get_os_command_output({
-      --       'git',
-      --       'rev-parse',
-      --       '--abbrev-ref',
-      --       'HEAD',
-      --     })[1]
-      --   end
-      --   if branch then
-      --     return vim.loop.cwd() .. '-' .. branch
-      --   else
-      --     return vim.loop.cwd()
-      --   end
-      -- end,
     },
   },
   keys = {
     {
       '<leader>a',
       function()
-        require('harpoon'):list():add()
+        require('harpoon'):list(key_name()):add()
       end,
       desc = 'harpoon file',
-    },
-    {
-      '<leader>sh',
-      function()
-        local harpoon = require 'harpoon'
-        local conf = require('telescope.config').values
-        local function toggle_telescope(harpoon_files)
-          local file_paths = {}
-          for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-          end
-
-          require('telescope.pickers')
-            .new({}, {
-              prompt_title = 'Harpoon',
-              finder = require('telescope.finders').new_table {
-                results = file_paths,
-              },
-              previewer = conf.file_previewer {},
-              sorter = conf.generic_sorter {},
-            })
-            :find()
-        end
-        -- harpoon.ui:toggle_quick_menu(harpoon:list())
-        toggle_telescope(harpoon:list())
-      end,
-      desc = 'harpoon quick menu',
     },
     {
       '<leader>h',
       function()
         local harpoon = require 'harpoon'
-        harpoon.ui:toggle_quick_menu(harpoon:list())
+        harpoon.ui:toggle_quick_menu(harpoon:list(key_name()))
       end,
       desc = 'harpoon quick menu',
     },
     {
       '<leader>1',
       function()
-        require('harpoon'):list():select(1)
+        require('harpoon'):list(key_name()):select(1)
       end,
       desc = 'harpoon to file 1',
     },
     {
       '<leader>2',
       function()
-        require('harpoon'):list():select(2)
+        require('harpoon'):list(key_name()):select(2)
       end,
       desc = 'harpoon to file 2',
     },
     {
       '<leader>3',
       function()
-        require('harpoon'):list():select(3)
+        require('harpoon'):list(key_name()):select(3)
       end,
       desc = 'harpoon to file 3',
     },
     {
       '<leader>4',
       function()
-        require('harpoon'):list():select(4)
+        require('harpoon'):list(key_name()):select(4)
       end,
       desc = 'harpoon to file 4',
     },
     {
       '<leader>5',
       function()
-        require('harpoon'):list():select(5)
+        require('harpoon'):list(key_name()):select(5)
       end,
       desc = 'harpoon to file 5',
     },
     {
       '<leader>6',
       function()
-        require('harpoon'):list():select(6)
+        require('harpoon'):list(key_name()):select(6)
       end,
       desc = 'harpoon to file 6',
     },
     {
       '<leader>7',
       function()
-        require('harpoon'):list():select(7)
+        require('harpoon'):list(key_name()):select(7)
       end,
       desc = 'harpoon to file 7',
     },
     {
       '<leader>8',
       function()
-        require('harpoon'):list():select(8)
+        require('harpoon'):list(key_name()):select(8)
       end,
       desc = 'harpoon to file 8',
     },
     {
       '<leader>9',
       function()
-        require('harpoon'):list():select(9)
+        require('harpoon'):list(key_name()):select(9)
       end,
       desc = 'harpoon to file 9',
-    },
-    {
-      '<leader>0',
-      function()
-        require('harpoon'):list():select(0)
-      end,
-      desc = 'harpoon to file 0',
     },
   },
 }
