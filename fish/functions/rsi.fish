675a3454891458b84802b7dcc2a32b936747871d
David Benton <djb@dajabe.nz>
2024-10-15 11:43:54 +1300

    small tweaks to rsi function

--- fish/functions/rsi.fish
+++ fish/functions/rsi.fish
@@ -1,25 +1,23 @@
 function rsi
-    # Capture the output in a file
     set -l temp_file (mktemp)
     rspec $argv | tee $temp_file >/dev/tty
 
-    # Extract image paths
     set diff_image (cat $temp_file | grep -A 1 'View Image Diff:' | tail -n 1 | string trim)
     set generated_image (cat $temp_file | grep -A 1 'View Generated Image:' | tail -n 1 | string trim)
     set expected_image (cat $temp_file | grep -A 1 'View Expected Image:' | tail -n 1 | string trim)
 
-    # Clean up
     rm $temp_file
 
     function open_if_exists
         if test -f $argv[1]
             # change this to the appropriate open command for your setup i.e. MacOS might be open
-            nomacs -m frameless $argv[1]
+            nomacs $argv[1]
         else
             echo "File not found: $argv[1]"
         end
     end
 
+    echo ""
     echo "Which files would you like to open?"
     echo "N - None"
     echo "A - All"
@@ -27,6 +25,7 @@ function rsi
     echo "G - Generated Image"
     echo "E - Expected Image"
     echo "Or any two-letter combination from DGE"
+    echo ""
 
     read -P "Enter your choice(N): " user_choice
 
