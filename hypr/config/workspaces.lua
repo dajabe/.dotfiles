92e33296197eb33e9adebfd15597b402b6b542f8
David Benton <djb@dajabe.nz>
2026-07-28 17:24:59 +1200

    Fix the borkedness

--- hypr/config/workspaces.lua
+++ hypr/config/workspaces.lua
@@ -1,10 +1,10 @@
 local v = require("config.vars")
 
--- HDMI-A-1: right monitor
+-- DP-2: left / primary monitor
 hl.workspace_rule({
     workspace = "1",
     default_name = "web",
-    monitor = "HDMI-A-1",
+    monitor = "DP-2",
     default = true,
     on_created_empty = v.browser,
 })
@@ -12,31 +12,31 @@ hl.workspace_rule({
 hl.workspace_rule({
     workspace = "2",
     default_name = "shell",
-    monitor = "HDMI-A-1",
+    monitor = "DP-2",
     on_created_empty = v.terminal,
 })
 
 hl.workspace_rule({
     workspace = "3",
-    monitor = "HDMI-A-1",
+    monitor = "DP-2",
 })
 
 hl.workspace_rule({
     workspace = "4",
-    monitor = "HDMI-A-1",
+    monitor = "DP-2",
 })
 
--- DP-2: left monitor
+-- HDMI-A-1: right / secondary monitor
 hl.workspace_rule({
     workspace = "5",
     default_name = "music",
-    monitor = "DP-2",
+    monitor = "HDMI-A-1",
     default = true,
 })
 
 for workspace = 6, 9 do
     hl.workspace_rule({
         workspace = tostring(workspace),
-        monitor = "DP-2",
+        monitor = "HDMI-A-1",
     })
 end
