bd177c88a905be62d4476b2e7554208282da95d4
David Benton <djb@dajabe.nz>
2026-07-30 10:19:53 +1200

    Add a wayle config

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -1,6 +1,7 @@
 hl.on("hyprland.start", function()
-    hl.exec_cmd("systemctl --user start hyprpolkitagent")
-    hl.exec_cmd("/usr/bin/waybar")
+    -- hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    -- wayle is launched by the user service
+    -- hl.exec_cmd("wayle shell")
     hl.exec_cmd("/opt/1Password/1password --silent")
 end)
 
