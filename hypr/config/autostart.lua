0ce86b8f3f7780c958f55385ec757a704808bc7c
David Benton <djb@dajabe.nz>
2026-08-22 04:15:02 +1200

    Update wayle and add signing back

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -1,7 +1,2 @@
-hl.on("hyprland.start", function()
-    -- hl.exec_cmd("systemctl --user start hyprpolkitagent")
-    -- wayle is launched by the user service
-    -- hl.exec_cmd("wayle shell")
-    hl.exec_cmd("/opt/1Password/1password --silent")
-end)
+-- Wayle and 1Password are launched by user services.
 
